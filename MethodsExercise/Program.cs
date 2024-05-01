2ea4fc72e36278fd2fabd3d06519a8280acd1884
jessvand <jmvandbeek@gmail.com>
2024-04-30 22:00:55 -0400

    Had funsies with words

--- MethodsExercise/Program.cs
+++ MethodsExercise/Program.cs
@@ -6,5 +6,21 @@
         {
             Console.WriteLine("Hello, World!");
         }
+
+        public static void FunSies()
+        {
+            Console.WriteLine("Who is your favorite person?");
+            string yourPerson = Console.ReadLine();
+            Console.WriteLine($"Great! What is your favorite person's favorite color?");
+            string favColor = Console.ReadLine();
+            Console.WriteLine("What is their favorite food?");
+            string favFood = Console.ReadLine();
+            Console.WriteLine("What is their best quality?");
+            string favQual = Console.ReadLine();
+
+            Console.WriteLine($"Yesterday, I ran into {yourPerson} and they were wearing a hideous {favColor} shirt. The gorss thing is that it was covered in {favFood}. Although {favQual}, they still smelled like {favFood}");
+;        }
+
+
     }
 }
